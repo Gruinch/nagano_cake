b4cd47507619c95666695329ef3781fd0933b921
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-01 12:15:40 +0000

    [add] database for admin and customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
