942ff27e1e1708415c4694bf94eb3945c423c757
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-02 15:50:22 +0000

    [add]cart_items table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_all'
+    get 'cart_items/create'
+  end
   namespace :admin do
     get 'genres/index'
     get 'genres/create'
