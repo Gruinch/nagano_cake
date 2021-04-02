cc8d33a5901153102da2c95d85864f0671a17455
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-02 23:45:00 +0000

    [add]relations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to:genres
+  has_many:cart_items, dependent: :destroy
+  has_many:order_details, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+    namespace :public do
+    get 'homes/top'
+    get 'home/about'
+    end
+
+    namespace :admin do
+    get '/' => 'homes#top', as: '/homes/top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items
+    resources :genres, only: [:index, :create, :edit, :update]
+    get 'orders/show'
+    get 'orders/update'
+    get 'order_details/update'
+    end
+
 
   devise_for :customers, controllers: {
     sessions:      'customers/sessions',
@@ -12,21 +27,10 @@ Rails.application.routes.draw do
     registrations: 'admin/registrations',
   }
 
-    get '/' => 'homes#top', as: 'homes/top'
-    get '/about' => 'homes#about', as: 'homes/about'
     resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
     resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :edit, :update, :destroy]
     resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
     resources :orders
 
-  namespace :admin do
-    get '/' => 'homes#top', as: '/homes/top'
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :items
-    resources :genres, only: [:index, :create, :edit, :update]
-    get 'orders/show'
-    get 'orders/update'
-    get 'order_details/update'
-  end
 end
