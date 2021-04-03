054c67f8bcf890730dcc1adcff7d9981d9be07a5
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-03 14:04:46 +0000

    added items new and items show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,22 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items=Item.includes(:genre)
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.genre_id = 0
+    # binding.pry
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -16,4 +24,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to:genres
+  belongs_to:genres, optional: true
   has_many:cart_items, dependent: :destroy
   has_many:order_details, dependent: :destroy
+  attachment :image
 end
