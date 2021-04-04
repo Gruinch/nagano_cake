11d21448bf7c85f02a561a9646b97154375524c0
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-04 10:40:47 +0000

    [add]Customer index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,27 @@
     email: 'nagano@email.com',
     password: 'naganocake',
     )
+
+  Customer.create!(
+    email: 'hello@email.com',
+    password: 'test123',
+    last_name: '熊本',
+    first_name: 'くまこ',
+    last_name_kana: 'クマモト',
+    first_name_kana: 'クマコ',
+    postal_code: '111-1111',
+    address: '沖縄県那覇市',
+    telephone_number: '000-0000-0000'
+    )
+
+  Customer.create!(
+    email: 'okay@test.com',
+    password: 'test1234',
+    last_name: '空腹',
+    first_name: '桃子',
+    last_name_kana: 'クウフク',
+    first_name_kana: 'モモコ',
+    postal_code: '111-1112',
+    address: '沖縄県名護市',
+    telephone_number: '000-0000-0001'
+  )
