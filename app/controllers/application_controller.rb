f05d7494eaa14154def92f15a91dbc6ff60dc830
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-10 11:36:10 +0000

    add customer sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!, except: [:new]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana,
+    :email, :password, :postal_code, :address, :telephone_number, :is_active])
+  end
 
 
   def after_sign_in_path_for(resource)
@@ -9,7 +17,7 @@ class ApplicationController < ActionController::Base
     when Admin
       admin_homes_top_path
     when Customer
-      public_homes_top_path
+      root_path
 
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
     scope module: :public do
     root 'homes#top'
     get '/about' => 'homes#about', as: '/homes/about'
-    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+
+    devise_for :customers, controllers: {
+    sessions:      'public/customers/sessions',
+    passwords:     'public/customers/passwords',
+    registrations: 'public/customers/registrations'
+  }
+    resources :customers
     resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :edit, :update, :destroy]
     resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
     resources :orders
+
     end
 
     namespace :admin do
@@ -17,13 +24,6 @@ Rails.application.routes.draw do
     resources :orders, only: [:show, :index, :update]
     get 'order_details/update'
     end
-
-
-  devise_for :customers, controllers: {
-    sessions:      'customers/sessions',
-    passwords:     'customers/passwords',
-    registrations: 'customers/registrations'
-  }
 
   devise_for :admins, controllers: {
     sessions:      'admin/sessions',
